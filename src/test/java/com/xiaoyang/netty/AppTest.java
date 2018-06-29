25001bff65022f26bfa2c92d4896b5e65dc8b298
xiaoyang <1483863489@qq.com>
2018-06-29 16:19:27 +0800

    Initial commit

---
+++ src/test/java/com/xiaoyang/netty/AppTest.java
@@ -0,0 +1,12 @@
+package com.xiaoyang.netty;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest  {
+
+}
