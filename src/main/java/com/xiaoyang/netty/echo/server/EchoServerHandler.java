6bf094b417d8d9e4e12069205877e6884fdabbfb
xiaoyang <1483863489@qq.com>
2018-08-31 17:27:23 +0800

    添加server端

---
+++ src/main/java/com/xiaoyang/netty/echo/server/EchoServerHandler.java
@@ -0,0 +1,30 @@
+package com.xiaoyang.netty.echo.server;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * @author WXY
+ * @date 2018/8/31 17:24
+ */
+
+public class EchoServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        ctx.write(msg);
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) {
+        ctx.flush();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        // Close the connection when an exception is raised.
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+}
---
+++ src/main/java/com/xiaoyang/netty/echo/server/Echoserver.java
@@ -0,0 +1,71 @@
+package com.xiaoyang.netty.echo.server;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.handler.ssl.util.SelfSignedCertificate;
+
+
+/**
+ * 非常基本的客户端和服务器
+ *
+ * @author WXY
+ * @date 2018/8/31 16:58
+ */
+public class Echoserver {
+    /**
+     * 获取系统配置
+     */
+    private static final Boolean SSL = System.getProperty("ssl") != null;
+    private static final int PORT = Integer.parseInt(System.getProperty("port", "8007"));
+    public static void main(String[] args) throws Exception {
+        //SSL配置
+        final SslContext sslContext;
+        if (SSL) {
+            SelfSignedCertificate signedCertificate = new SelfSignedCertificate();
+            sslContext = SslContextBuilder.forServer(signedCertificate.certificate(), signedCertificate.privateKey()).build();
+        } else {
+            sslContext = null;
+        }
+        // Configure the server.
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        final EchoServerHandler serverHandler = new EchoServerHandler();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 100)
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        public void initChannel(SocketChannel ch) throws Exception {
+                            ChannelPipeline p = ch.pipeline();
+                            if (sslContext != null) {
+                                p.addLast(sslContext.newHandler(ch.alloc()));
+                            }
+                            //p.addLast(new LoggingHandler(LogLevel.INFO));
+                            p.addLast(serverHandler);
+                        }
+                    });
+
+            // Start the server.
+            ChannelFuture f = b.bind(PORT).sync();
+
+            // Wait until the server socket is closed.
+            f.channel().closeFuture().sync();
+        } finally {
+            // Shut down all event loops to terminate all threads.
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+
+    }
+
+}
