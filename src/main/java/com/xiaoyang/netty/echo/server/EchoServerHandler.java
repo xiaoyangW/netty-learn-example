93f8d4373f90f3babc56e9dc600aaa981c03db03
xiaoyang <1483863489@qq.com>
2018-11-14 20:32:49 +0800

    echo服务

--- src/main/java/com/xiaoyang/netty/echo/server/EchoServerHandler.java
+++ src/main/java/com/xiaoyang/netty/echo/server/EchoServerHandler.java
@@ -4,6 +4,7 @@ import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
 
 /**
  * @author WXY
@@ -12,11 +13,27 @@ import io.netty.channel.ChannelInboundHandlerAdapter;
 @ChannelHandler.Sharable
 public class EchoServerHandler extends ChannelInboundHandlerAdapter {
 
+    /**
+     * 调查收到的数据
+     */
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) {
         //默认丢弃服务
         System.out.println("msg----" + msg);
-        System.out.println(((ByteBuf) msg).capacity());
+        /*try {
+            ByteBuf byteBuf = (ByteBuf) msg;
+            while (byteBuf.isReadable()){
+                System.out.println(byteBuf.readByte());
+                System.out.flush();
+            }
+        } catch (Exception e){
+            ReferenceCountUtil.release(msg);
+        }*/
+        //Echo服务器
+        ctx.write(msg);
+        ctx.flush();
+
+
     }
 
     @Override
